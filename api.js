90e98daa59ade5fa4d0de2f37165276a727c5b0a
Blake Tsuzaki <dtsuzaki@scu.edu>
2018-03-01 09:35:05 -0800

    Moved authentication to URL

--- api.js
+++ api.js
@@ -39,15 +39,13 @@ function API() {
     var _initializeWatson = function(callback) {
         var request = new XMLHttpRequest();
         request.onreadystatechange = function() {
+            console.log(request.response);
             if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
                 typeof callback == 'function' && callback(request.responseText);
             }
         };
-        request.open('GET', 'https://stream.watsonplatform.net/authorization/api/v1/token?url=https://stream.watsonplatform.net/text-to-speech/api');
+        request.open('GET', 'https://8d444664-2dd9-4aee-a3cd-69f3bcb1c0f9:eLJQtO1GiNvK@stream.watsonplatform.net/authorization/api/v1/token?url=https://stream.watsonplatform.net/text-to-speech/api');
         request.setRequestHeader("Content-type", "text/plain");
-        request.setRequestHeader("Access-Control-Request-Method", "GET");
-        request.setRequestHeader("Access-Control-Request-Headers", "Authorization")
-        request.setRequestHeader("Authorization", "Basic " + btoa("8d444664-2dd9-4aee-a3cd-69f3bcb1c0f9:eLJQtO1GiNvK"));
         request.send();
     };
 
